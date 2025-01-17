30b6db1a6013ee6da2bfd7ef35b8391f6e0ab2aa
harsh24062 <129416678+harsh24062@users.noreply.github.com>
2025-01-17 20:50:55 +0530

    checking colors

--- src/components/ChatPage.jsx
+++ src/components/ChatPage.jsx
@@ -31,7 +31,7 @@ const ChatPage = () => {
         <div className=" border fixed bottom-0 w-full h-12 bg-gray-400 dark:bg-gray-600 border-white dark:border-gray-700 ">
             <div className="h-full px-2 gap-2 rounded-md mx-auto  w-2/3 dark:border-gray-700 flex justify-between items-center">
             <input type="text" placeholder="Type your message here..." className=" w-full border dark:border-gray-600 dark:bg-gray-500 px-3 py-2 rounded  h-full focus:outline-none focus:ring-1 focus:ring-green-400"/>
-            <button className="bg-red-500 dark:bg-blue-600 px-4 py-3 rounded-xl hover:bg-green-500 hover:scale-105 transition-all duration-200"> <MdSend size={20}/> </button>
+            <button className="bg-red-500 dark:bg-blue-500 px-4 py-3 rounded-xl hover:bg-green-500 hover:scale-105 transition-all duration-200"> <MdSend size={20}/> </button>
             </div>
         </div>
 
