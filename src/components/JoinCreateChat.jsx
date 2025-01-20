484c7d2a73dbffcd91fee4fc53a5b97ff81763b6
harsh24062 <129416678+harsh24062@users.noreply.github.com>
2025-01-21 01:34:39 +0530

    worked on axios for sending api calls and done creatimg room part from frontend 1st join/create page , now have to work on to join chat

--- src/components/JoinCreateChat.jsx
+++ src/components/JoinCreateChat.jsx
@@ -1,6 +1,62 @@
+import { useState } from "react"
 import ChatIcon from "../assets/chat.png"
+import toast from "react-hot-toast";
+import { createRoomApi } from "../Services/RoomService";
 
 const JoinCreateChat = () => {
+
+  const [detail,setDetail] = useState({
+    roomId:"",
+    userName:"" 
+  });
+
+
+  function handleInputFormChange(event){
+   setDetail({
+    ...detail,   // The spread operator (...) is used to copy all the properties from an existing object or array into a new object or array.
+                 //Without the spread operator, only the updated key-value pair would remain in the state, and all other properties would be lost.
+    [event.target.name]:event.target.value,
+   })
+  }
+
+  function validateForm(){
+    if(detail.userName=="" || detail.roomId==""){
+      toast.error("Name or Room Id is missing!!")
+      return false;
+    }
+    return true;
+  }
+
+  function joinRoom(){
+
+     if(validateForm()){
+      //joinroom
+     }
+  }
+
+  async function createRoom(){
+     
+    if(validateForm()){
+      //create room
+        console.log(detail);
+      // call api(backend) to create room 
+      try{
+        const response = await createRoomApi(detail.roomId);
+        console.log(response);
+        toast.success("Room created successfully");
+        joinRoom();
+
+      }catch(error){
+         if(error.status==400){
+          toast.error("This RoomId already Used by SomeOne, Try new RoomId")
+         }
+         else{
+          console.log("error in room creation")
+         }
+      }
+    }
+  }
+
   return (
 
     <div className="min-h-screen flex justify-center items-center ">
@@ -22,7 +78,14 @@ const JoinCreateChat = () => {
            <label htmlFor="name" className="block mb-2 text-left text-xl font-medium font-serif text-gray-200 hover:text-yellow-300 ">
             Your Name
             </label>
-            <input type="text" id="name" className="w-full dark:text-black bg-orange-100 border-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-white"/>
+            <input
+             onChange={handleInputFormChange}
+             value={detail.userName}
+             name="userName"
+             placeholder="Enter your name..."
+             type="text" id="name"
+             className="w-full dark:text-black bg-orange-100 border-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-white"
+             />
         </div>
 
         {/* roomId div*/}
@@ -30,17 +93,23 @@ const JoinCreateChat = () => {
            <label htmlFor="roomId" className="block mb-2 text-left text-xl font-medium font-serif text-gray-200 hover:text-yellow-300 ">
             Room Id
             </label>
-            <input type="text" id="roomId" className="w-full dark:text-black bg-orange-100 border-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-white"/>
+            <input 
+            onChange={handleInputFormChange}
+            value={detail.roomId}
+            name="roomId"
+            placeholder="Enter roomId here..."
+              type="text" id="roomId" className="w-full dark:text-black bg-orange-100 border-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-white"
+             />
         </div>
 
         {/* button */}
 
         <div className="flex justify-center gap-14">
-          <button className="px-2 py-3 bg-emerald-500 text-white font-sans text-1xl font-semibold rounded-md hover:bg-blue-300 hover:text-black hover:text-lg hover:border-e-lime-500">
+          <button onClick={joinRoom} className="px-2 py-3 bg-emerald-500 text-white font-sans text-1xl font-semibold rounded-md hover:bg-blue-300 hover:text-black hover:text-lg hover:border-e-lime-500">
             Join Room
           </button>
             
-          <button className="px-2 py-3 bg-red-600 text-white font-sans text-1xl font-semibold rounded-md hover:bg-blue-300 hover:text-black hover:text-lg hover:border-e-lime-500">
+          <button onClick={createRoom} className="px-2 py-3 bg-red-600 text-white font-sans text-1xl font-semibold rounded-md hover:bg-blue-300 hover:text-black hover:text-lg hover:border-e-lime-500">
             Create Room 
           </button>
         </div>
